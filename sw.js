f6d111fdc9ace73e2050c9cb14d22bf3fc6750d4
Sandro <sandro.a007@gmail.com>
2026-09-07 22:19:13 +0200

    Rendimiento sep-2026: caché 2026-09-08-c (parada total del planetario + tope DPR)

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@
    ============================================================ */
 'use strict';
 
-const VERSION = '2026-09-08-b';
+const VERSION = '2026-09-08-c';
 const CACHE_ESTATICA = 'manolito-estatica-' + VERSION;
 const CACHE_DINAMICA = 'manolito-dinamica-' + VERSION;
 const MAX_ENTRADAS_ESTATICAS = 600; // tiles incluidos; tope de seguridad
